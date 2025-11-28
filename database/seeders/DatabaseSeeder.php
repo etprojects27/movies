e096a451b557e391b2aff96217e121a420ecb42f
etprojects27 <96521480+etprojects27@users.noreply.github.com>
2025-11-28 14:37:55 +0200

    Update DatabaseSeeder.php
    I forgot to add the call to the seeder files.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->call(ArtistSeeder::class);
+        $this->call(MovieSeeder::class);
+        $this->call(MovieArtistSeeder::class);
     }
 }
